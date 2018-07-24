57e80a6904b933d587ad25bc683c83cbafbb2b9d
Aleksandr Ivasev <aleksandr.ivasev@simbirsoft.com>
2018-07-24 10:02:07 +0400

    first les

---
+++ ios-kurs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios-kurs
+//
+//  Created by user on 21.07.2018.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
