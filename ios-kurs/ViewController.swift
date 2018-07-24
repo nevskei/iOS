b70689d0329748df7d805e0b68d8c84c1d5e698e
Aleksandr Ivasev <aleksandr.ivasev@simbirsoft.com>
2018-07-24 21:59:27 +0400

    firebase

--- ios-kurs/ViewController.swift
+++ ios-kurs/ViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var nameTextField: UITextField!
+    @IBAction func TwitterButton(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var myImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var ref: DatabaseReference!
+        
+        ref = Database.database().reference()
+        
+        ref.child("user").child("1").observeSingleEvent(of: .value, with: {(snapshot) in
+            let value = snapshot.value as? NSDictionary
+            let username = value?["name"]
+            let photo = value?["src"]
+            self.name.text = username as? String;
+            
+            if let filePath = Bundle.main.path(forResource: photo as? String, ofType: "jpg"), let image = UIImage(contentsOfFile: filePath) {
+                self.myImage.contentMode = .scaleAspectFit
+                self.myImage.image = image
+            }
+        })
         // Do any additional setup after loading the view, typically from a nib.
     }
 
